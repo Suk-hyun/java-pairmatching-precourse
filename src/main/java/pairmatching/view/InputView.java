9e09a81ff3b1e9dcb3ddb892036382bf33c64b7c
심석현 <shim9597@gmail.com>
2023-09-16 23:52:55 +0900

    feat: 과정, 레벨, 미션 선택

---
+++ src/main/java/pairmatching/view/InputView.java
@@ -0,0 +1,42 @@
+package pairmatching.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private enum ConsoleMessage {
+        FUNCTION_CHOICE("기능을 선택하세요.\n" +
+                "1. 페어 매칭\n" +
+                "2. 페어 조회\n" +
+                "3. 페어 초기화\n" +
+                "Q. 종료"),
+        FUNCTION_INFO("#############################################\n" +
+            "과정: 백엔드 | 프론트엔드\n" +
+            "미션:\n" +
+            "  - 레벨1: 자동차경주 | 로또 | 숫자야구게임\n" +
+            "  - 레벨2: 장바구니 | 결제 | 지하철노선도\n" +
+            "  - 레벨3: \n" +
+            "  - 레벨4: 성능개선 | 배포\n" +
+            "  - 레벨5: \n" +
+            "############################################\n" +
+            "과정, 레벨, 미션을 선택하세요.\n" +
+            "ex) 백엔드, 레벨1, 자동차경주");
+
+        private final String message;
+
+        ConsoleMessage(String message) {
+            this.message = message;
+        }
+    }
+
+
+    public String readFunctionChoice() {
+        System.out.println(ConsoleMessage.FUNCTION_CHOICE.message);
+        return Console.readLine();
+    }
+
+    public String readCourseLevelMission() {
+        System.out.println(ConsoleMessage.FUNCTION_INFO.message);
+        return Console.readLine();
+    }
+}
